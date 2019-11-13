f61f67a21326e3e40faeac4deaa9a036d152ab6e
Yehoraz <46538668+Yehoraz@users.noreply.github.com>
2019-11-14 01:28:59 +0200

    commit

---
+++ src/main/java/com/Test/AngularAndSpring/AngularAndSpringApplication.java
@@ -0,0 +1,13 @@
+package com.Test.AngularAndSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AngularAndSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AngularAndSpringApplication.class, args);
+	}
+
+}
