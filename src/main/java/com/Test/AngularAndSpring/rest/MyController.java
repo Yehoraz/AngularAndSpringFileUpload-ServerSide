4da6c77cb5590d9746df7b40d90a00fad4222e33
Yehoraz <46538668+Yehoraz@users.noreply.github.com>
2019-11-23 19:47:10 +0200

    commit

---
+++ src/main/java/com/Test/AngularAndSpring/rest/MyController.java
@@ -0,0 +1,33 @@
+package com.Test.AngularAndSpring.rest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+public class MyController {
+
+	private final String baseFileDestination = "C:\\example\\example2\\Documents";
+
+	@PostMapping(value = "/addFile")
+	public ResponseEntity<?> addFile(@RequestParam("file") MultipartFile file) {
+		String directoryString = baseFileDestination + "\\" + file.getOriginalFilename();
+		File directory = new File(directoryString);
+		if (!directory.exists()) {
+			directory.mkdir();
+		}
+		directory = new File(directoryString + "\\" + file.getOriginalFilename());
+		try {
+			file.transferTo(directory);
+		} catch (IllegalStateException | IOException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("error");
+		}
+		return ResponseEntity.ok("File added");
+	}
+}
